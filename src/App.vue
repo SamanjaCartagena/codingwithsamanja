59498761cb0aad4878c8f7f1d614bd7c542ae5cf
SamanjaCartagena <cart.samanja09@gmail.com>
2022-12-16 13:19:00 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import Background from './views/Background.vue'
+</script>
+
+<template>
+  <div>
+    <the-header></the-header>
+    <background></background>
+  </div>
+</template>
+
+<style>
+body{
+  margin:0;
+  padding:0;
+}
+</style>
---
+++ src/views/Background.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container{
+  height:600px;
+  width:100%;
+  background-image: url(../assets/background.jpg);
+  background-repeat: none;
+  background-size:100%;
+}
+body{
+  padding:0;
+  margin:0;
+}
+</style>
